2e2c0a5023cfc1a9445c91e5de4cc01c3a49da17
Saikat Mandal <2727801@tcs.com>
2025-10-28 15:11:37 +0530

    updated colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,24 +3,28 @@ import { Card } from "@/components/ui/card";
 import { Funnel, Heart, Plus } from "lucide-react";
 import Image from "next/image";
 import mainImage from "@/app/assets/gta.jpeg";
-
+// #f18a10
+// #f4f5fc
 export default function Home() {
   return (
-    <div className="p-4">
-      <h1 className="text-3xl font-semibold">Catalogue</h1>
+    <div className="p-4 bg-[#f4f5fc]">
+      <header className="py-4">
+          <h1 className="text-3xl font-semibold">Ultragear</h1>
+      </header>
+      <h1 className="text-2xl mt-4 font-semibold">Catalogue</h1>
 
       {/* Filter Buttons */}
       <div className="flex flex-wrap items-center gap-2 md:flex-row mt-6">
-        <Button>
-          <Funnel />
+        <Button className="bg-[#ffffff] text-black hover:bg-[#f18a10] rounded-xl">
+          <Funnel /> Filter
         </Button>
-        <Button variant="outline">PS5</Button>
-        <Button variant="outline">PS4</Button>
-        <Button variant="outline">Xbox</Button>
+        <Button className="rounded-xl text-[#ffffff] bg-[#f18a10] hover:bg-orange-400" variant="secondary">PS5</Button>
+        <Button className="rounded-xl text-[#ffffff] bg-[#f18a10] hover:bg-orange-400" variant="secondary">PS4</Button>
+        <Button className="rounded-xl bg-[#ffffff]" variant="secondary">Xbox</Button>
       </div>
 
       {/* Main Content */}
-      <div className="mt-6 min-h-screen overflow-x-auto cursor-pointer">
+      <div className="mt-6 min-h-screen overflow-x-auto cursor-pointer p-4">
         <div
           className="
             grid grid-cols-2 gap-4
@@ -31,25 +35,27 @@ export default function Home() {
           "
         >
           {/* Example Card */}
-          <Card className="w-56 md:w-auto border-2 rounded-xl overflow-hidden hover:scale-105 transition-transform duration-300 ease-in-out p-4">
+          <Card className="w-46 md:w-auto border-2 rounded-xl overflow-hidden hover:scale-105 transition-transform duration-300 ease-in-out p-4 bg-[#ffffff]">
             <div className="flex justify-between mb-2">
               <Heart className="text-gray-600 hover:text-red-500 transition-colors" />
               <Plus className="text-amber-500" />
             </div>
 
-            {/* ✅ Responsive Image Container */}
-            <div className="relative w-full aspect-[3/4] md:aspect-[4/5] overflow-hidden rounded-lg">
+            {/*  Responsive Image Container */}
+            <div className="relative w-full  md:aspect-[4/5] overflow-hidden rounded-lg">
               <Image
                 src={mainImage}
                 alt="GTA V Cover"
-                fill
+                width={800}
+                height={600}
+                // fill
                 className="object-contain md:object-cover"
               />
             </div>
 
             <div className="flex flex-col items-center py-3">
               <p className="font-semibold">GTA V</p>
-              <p className="text-sm text-gray-500">₹ 299/-</p>
+              <p className="text-sm text-gray-500">from ₹ 299/-</p>
             </div>
           </Card>
         </div>
